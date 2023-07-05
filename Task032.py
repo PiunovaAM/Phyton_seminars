460e6b25ed6aead5e89800e816a12cfa53d0c528
Настя <Piu@192.168.1.3>
2023-07-05 16:15:44 +0300

    Add Task032

--- Task032.py
+++ Task032.py
@@ -3,6 +3,16 @@
 # Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
 # Вывод: [1, 9, 13, 14, 19]
 
+
+import random
+print (list_1 := [random.randint(- 10,10) for i in range(20)])
+max = 10
+min = 4
+for i in range(len(list_1)):
+	if min <= list_1[i] <= max:
+		print(i, end=' ')
+
+
 # print (list_1 := [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7])
 # list_2 = []
 # max = 10
@@ -29,12 +39,3 @@
 # 	if min <= list_1[i] <= max:
 # 		list_2.append(i)
 # print(list_2)
-
-import random
-print (list_1 := [random.randint(- 10,10) for i in range(20)])
-max = 10
-min = 4
-for i in range(len(list_1)):
-	if min <= list_1[i] <= max:
-		print(i, end=' ')
-		
