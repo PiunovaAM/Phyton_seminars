02093237ef4ab25204a0d4e5468e36be5713dd5a
Настя <Piu@192.168.1.3>
2023-07-10 10:49:31 +0300

    Modified Task031

--- Task031.py
+++ Task031.py
@@ -2,9 +2,99 @@
 # a0 = 0, a1 = 1, ak = ak-1 + ak-2 (k > 1).
 # Требуется найти N-е число Фибоначчи
 
-def fibonachi(n):
-    if n == 1 or n == 2:
-        return 1
-    return fibonachi(n-1) + fibonachi(n-2)
+# def fibonachi(n):
+#     if n == 1 or n == 2:
+#         return 1
+#     return fibonachi(n-1) + fibonachi(n-2)
 
-print(fibonachi(10))
+# print(fibonachi(10))
+
+
+# В списке хранятся числа. Нужно выбрать только чётные числа и составить список пар
+# (число; квадрат числа).
+# Пример: 1 2 3 5 8 15 23 38
+# Получить: [(2, 4), (8, 64), (38, 1444)]
+
+# list_1 = [1, 2, 3, 5, 8, 15, 23, 38]
+# for i in list_1:
+# 	if i % 2 == 0:
+# 		print((i, i*i), end=' ')
+		
+# # ИЛИ
+
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# out = []
+# for i in data :
+#     if i % 2 == 0:
+#         out.append((i, i ** 2))
+# print(out)
+
+# # с помощью lambda
+
+# def select(f, col):
+#     return [f(x) for x in col]
+
+# def where(f, col):
+#     return [x for x in col if f(x)]
+
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# res = select(int, data)
+# print(res)
+# res = where(lambda x: x % 2 == 0, res)
+# print(res) # [2, 8, 38]
+# res = list(select(lambda x: (x, x ** 2), res))
+# print(res)
+
+# Функция map
+# Функция map() применяет указанную функцию к каждому элементу итерируемого объекта и
+# возвращает итератор с новыми объектами.
+
+
+# Есть набор данных. Функция map позволяет увеличить каждый объект на 10.
+
+# print (list_1 := [i for i in range (1, 20)])
+# list_1 = list(map(lambda i: i + 10, list_1))
+# print(list_1)
+
+# Задача: C клавиатуры вводится некий набор чисел, в качестве разделителя используется
+# пробел. Этот набор чисел будет считан в качестве строки. Как превратить list строк в list чисел?
+# 1. Маленькое отступление, функция строка.split() - убирает все пробелы и создаем список из
+# значений строки.
+
+data = '1 2 3 5 8 15 2'
+print(data)
+data = list(map(int, data.split()))
+print(data)
+
+# Функция filter
+# Функция filter() применяет указанную функцию к каждому элементу итерируемого объекта и
+# возвращает итератор с теми объектами, для которых функция вернула True.
+data = [x for x in range(10)]
+res = list(filter(lambda x: x % 2 == 0, data))
+print(res) # [0, 2, 4, 6, 8]
+
+
+# Функция zip
+# Функция zip() применяется к набору итерируемых объектов и возвращает итератор с кортежами
+# из элементов входных данных
+
+# Пример:
+users = ['user1', 'user2', 'user3', 'user4', 'user5']
+ids = [4, 5, 9, 14, 7]
+data = list(zip(users, ids))
+print(data) # [('user1', 4), ('user2', 5), ('user3', 9), ('user4', 14), ('user5', 7)]
+
+# Функция zip () пробегает по минимальному входящему набору:
+users = ['user1', 'user2', 'user3', 'user4', 'user5']
+ids = [4, 5, 9, 14, 7]
+salary = [111, 222, 333]
+data = list(zip(users, ids, salary))
+print(data) # [('user1', 4, 111), ('user2', 5, 222), ('user3', 333)]
+
+# Функция enumerate
+# Функция enumerate() применяется к итерируемому объекту и возвращает новый итератор с
+# кортежами из индекса и элементов входных данных.
+# Функция enumerate() позволяет пронумеровать набор данных.
+users = ['user1', 'user2', 'user3']
+data = list(enumerate(users))
+print(data) # [(0, 'user1'), (1, 'user2'), (2, 'user3))]
